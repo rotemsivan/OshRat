73f863c554f3b2ea859224126610a91ee76b75ef
Rotem <rotemsivan5@gmail.com>
2026-06-04 11:08:50 +0300

    Initial Commit

---
+++ OshRat/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  OshRat
+//
+//  Created by Rotem Sivan on 04/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ OshRatTests/OshRatTests.swift
@@ -0,0 +1,19 @@
+//
+//  OshRatTests.swift
+//  OshRatTests
+//
+//  Created by Rotem Sivan on 04/06/2026.
+//
+
+import Testing
+@testable import OshRat
+
+struct OshRatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
